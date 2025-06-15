86f5eca1eab9429d477af8f51b40879b5752cf8e
chaa-chaa1 <cth4701@naver.com>
2025-06-15 05:08:52 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
 
+uploaded_file = st.file_uploader("진주시 비상소화장치", type=["xlsx"])
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+
+    df = df.rename(columns={
+        '위도': 'lat',
+        '경도': 'lon'
+    })
+
+    st.map(df)
+
+    st.dataframe(df)
